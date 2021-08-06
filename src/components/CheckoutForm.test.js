2afdd17ce6731558228ead8b732fdb4d44631c9a
Johanna Rodriguez <>
2021-08-06 15:12:04 -0500

    test added

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,65 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
+
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.queryByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+  expect(header).toHaveTextContent(/checkout form/i);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  userEvent.type(firstNameInput, "Johanna");
+
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  userEvent.type(lastNameInput, "Rodriguez");
+
+  const addressInput = screen.getByLabelText(/address/i);
+  userEvent.type(addressInput, "asereje");
+
+  const cityInput = screen.getByLabelText(/city/i);
+  userEvent.type(cityInput, "chicago");
+
+  const stateInput = screen.getByLabelText(/state/i);
+  userEvent.type(stateInput, "des plaines");
+
+  const zipInput = screen.getByLabelText(/zip/i);
+  userEvent.type(zipInput, "60038");
+
+  const submitButton = screen.getByRole("button");
+  userEvent.click(submitButton);
+
+  const successMessageText = screen.getByTestId("successMessage")
+  expect(successMessageText).toHaveTextContent(
+    "You have ordered some plants! Woo-hoo!"
+  );
+
+  const firstNameDisplay = screen.queryByText(/Johanna/i);
+  expect(firstNameDisplay).toBeInTheDocument();
+
+  const lastNameDisplay = screen.queryByText(/Rodriguez/i);
+  expect(lastNameDisplay).toBeInTheDocument();
+
+  const addressDisplay = screen.queryByText(/asereje/i);
+  expect(addressDisplay).toBeInTheDocument();
+
+  const cityDisplay = screen.queryByText(/chicago/i);
+  expect(cityDisplay).toBeInTheDocument();
+
+  const stateDisplay = screen.queryByText(/des plaines/i);
+  expect(stateDisplay).toBeInTheDocument();
+
+  const zipDisplay = screen.queryByText(/60038/i);
+  expect(zipDisplay).toBeInTheDocument();
+
+  
+ 
+});
